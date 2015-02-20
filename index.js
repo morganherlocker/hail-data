6c403cdd9145dd4a5a1b83e0eb73d635728da1e2
morganherlocker <morgan.herlocker@gmail.com>
2015-02-20 00:18:55 -0500

    compute breaks and filter dead cells

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var turf = require('turf');
 turf.squareGrid = require('turf-square-grid');
 turf.triangleGrid = require('turf-triangle-grid');
 var fs = require('fs');
-
+/*
 var paths = JSON.parse(fs.readFileSync('./hail-paths.geojson'));
 
 console.log('calculating centroids..');
@@ -20,14 +20,14 @@ var tribin = turf.triangleGrid(bbox, 100, 'miles');
 var hexbin = turf.hex(bbox, 1);
 var squarebin = turf.squareGrid(bbox, 100, 'miles');
 
-console.log('processing tribin..');
+console.log('aggregating tribin..');
 tribin = timeExpand(tribin, hail, 'tribin');
-console.log('processing hexbin..');
+console.log('aggregating hexbin..');
 hexbin = timeExpand(hexbin, hail, 'hexbin');
-console.log('processing squarebin..');
+console.log('aggregating squarebin..');
 squarebin = timeExpand(squarebin, hail, 'squarebin');
 
-console.log('processing pointbin..');
+console.log('aggregating pointbin..');
 var pointbin = turf.featurecollection([]);
 pointbin.features = squarebin.features.map(function(square, k){
     var cent = turf.centroid(square);
@@ -35,12 +35,30 @@ pointbin.features = squarebin.features.map(function(square, k){
     return cent;
 });
 
+*/
+
+//////DELETE BLOCK
+var tribin = JSON.parse(fs.readFileSync('./tribin.geojson'));
+var hexbin = JSON.parse(fs.readFileSync('./hexbin.geojson'));
+var squarebin = JSON.parse(fs.readFileSync('./squarebin.geojson'));
+var pointbin = JSON.parse(fs.readFileSync('./pointbin.geojson'));
+//////
+
+console.log('quantile tribin..')
+tribin = normalize(tribin);
+console.log('quantile hexbin..')
+hexbin = normalize(hexbin);
+console.log('quantile squarebin..')
+squarebin = normalize(squarebin);
+console.log('quantile pointbin..')
+pointbin = normalize(pointbin);
+/*
 console.log('saving grids..');
 fs.writeFileSync('./tribin.geojson', JSON.stringify(tribin));
 fs.writeFileSync('./hexbin.geojson', JSON.stringify(hexbin));
 fs.writeFileSync('./squarebin.geojson', JSON.stringify(squarebin));
 fs.writeFileSync('./pointbin.geojson', JSON.stringify(pointbin));
-
+*/
 console.log('..complete');
 
 function timeExpand (grid, hail, name) {
@@ -61,4 +79,31 @@ function timeExpand (grid, hail, name) {
         return cell;
     });
     return expanded;
+}
+
+function normalize (grid) {
+    grid.features = grid.features.filter(function(cell){
+        var year = 1955;
+        while(year <= 2013){
+            if (cell.properties[year.toString()] > 0) return true;
+            year++;
+        }
+    });
+
+    var breaks = [];
+    var year = 1955;
+    while(year <= 2013){
+        var filtered = turf.featurecollection([])
+        filtered.features = grid.features.filter(function(cell){
+            if (cell.properties[year.toString()] > 0) return true;
+        });
+
+        var newBreak = {};
+        newBreak[year.toString()] = turf.quantile(filtered, year.toString(), [20,40,60,70,80,90,95,99]);
+        breaks.push(newBreak);
+        year++;
+    }
+    grid = grid.features.map(function(cell){
+
+    });
 }
